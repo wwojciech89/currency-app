9a022968850efa405a141fa28afab66122dd2c6f
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-04-14 10:53:04 +0200

    add styles to ProfileWrapper

--- src/assets/components/ProfileWrapper/ProfileWrapper.js
+++ src/assets/components/ProfileWrapper/ProfileWrapper.js
@@ -4,7 +4,16 @@ import styles from "../ProfileWrapper/ProfileWrapper.module.scss";
 
 const ProfileWrapper = () => (
 <div className={styles.wrapper}>
-    
+    <div className={styles.data__container}>
+        <h1>Profile</h1>
+        <p className={styles.name}>John Doe</p>
+        <p className={styles.city}>Gdańsk</p>
+    </div>
+    <div className={styles.picture__container}>
+        <div className={styles.background__picture}>
+            <div className={styles.picture}></div>
+        </div>
+    </div>
 </div>
 );
 

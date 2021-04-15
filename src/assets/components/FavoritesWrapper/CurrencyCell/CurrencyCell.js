fd805dee8a0ac82301ead8c5f9909aecce1f2b93
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-04-15 10:23:17 +0200

    add CurrencyCell component

---
+++ src/assets/components/FavoritesWrapper/CurrencyCell/CurrencyCell.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "../CurrencyCell/CurrencyCell.module.scss";
+
+const CurrencyCell = () => (
+    <div className={styles.cell__wrapper}>
+        <div className={styles.flag__wrapper}>
+            <div className={styles.flag}></div>
+        </div>
+        <div className={styles.text__wrapper}>
+            <p>EUR</p>
+            <p>Euro</p>
+            <p>4,56zł</p>
+        </div>
+        <div className={styles.button__wrapper}>
+            <div className={styles.delete}>Delete</div>
+        </div>
+    </div>
+)
+
+
+
+
+export default CurrencyCell;
--- src/assets/components/FavoritesWrapper/FavortiesWrapper.js
+++ src/assets/components/FavoritesWrapper/FavortiesWrapper.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from "../FavoritesWrapper/FavoritesWrapper.module.scss";
+import CurrencyCell from "./CurrencyCell/CurrencyCell"
 
 const FavoritesWrapper = () => {
     return (
     <div className={styles.wrapper}>
         <h2>Favorites Currency</h2>
+        <CurrencyCell />
         <a href="/">Add favorites</a>
     </div>
     )

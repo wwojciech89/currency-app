18772a27210c5c6bc44c087c84c7a8d6303cdaa8
Mariusz Wojtkiewicz <wojtkiewicz89@gmail.com>
2021-04-16 14:44:29 +0200

    add styles and pictures to currencies

--- src/assets/components/CurrencyWrapper/CurrencyElement/CurrencyElement.js
+++ src/assets/components/CurrencyWrapper/CurrencyElement/CurrencyElement.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from "../CurrencyElement/CurrencyElement.module.scss";
-
+import heartGreen from "../../../images/heart-green.png"
+import heartPink from "../../../images/heart-pink.png"
 
 const CurrencyElement = ({data}) => {
     console.log(data)
     return(
        <div className={styles.element}>
-        <p>{data.currency}</p>
-        <p>{data.code}</p>
-        <p>{data.mid}</p>
+        <p className={styles.name}>{data.currency}</p>
+        <p className={styles.code}>{data.code}</p>
+        <p className={styles.mid}>{data.mid}</p>
+        <img className={styles.picture} src={heartGreen} alt="heart"/>
     </div>)
 }
 
